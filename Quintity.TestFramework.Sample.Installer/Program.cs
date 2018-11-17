658548247949701ef4d7abb64045f5c3c9717823
James Mothershead <jmothershead@quintity.com>
2018-11-17 10:36:00 -0800

    Created uninstaller.

--- Quintity.TestFramework.Sample.Installer/Program.cs
+++ Quintity.TestFramework.Sample.Installer/Program.cs
@@ -13,17 +13,27 @@ namespace Quintity.TestFramework.Sample.Installer
         {
             var project = new Project("Quintity.Sample.Tests",
                 new Dir($@"C:\Quintity TestEngineer 3.5",
+
+                    new ExeFileShortcut("Uninstall.SampleAutomation", "[System64Folder]msiexec.exe", "/x [ProductCode] /q"),
+                    
                     new Dir("TestAssemblies",
-                        new DirFiles($@"...\Quintity.TestFramework.TestLibrary\bin\Debug\*.dll"))),
+                        new DirFiles($@"...\Quintity.TestFramework.TestLibrary\bin\Debug\*.dll"))
+                
+            ));
 
-                new ExeFileShortcut("Uninstall.AutomationSample", "[System64Folder]msiexec.exe", "/x [ProductCode] /q")
-                );
+            // Place output *.msi to appropriate bin folder.
+            project.OutDir = $@".\bin\Debug\";
 
+            // Suppress duplicate file warning
+            project.LightOptions += "-sice:ICE30";
 
-
-            project.OutDir = $@".\bin\Debug";
+            // Minimal UI
             project.UI = WUI.WixUI_ProgressOnly;
+
+            // Set installer GUID
             project.GUID = new Guid("7D54CC81-9CC6-4F95-98FC-7D2DC92C7105");
+
+            // Build this bad boy
             project.BuildMsi();
         }
     }
